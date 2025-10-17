e353f3c267c51e7ffdf188250a22f410081628cb
Pierre Gronnier <pierre.gronnier@gmail.com>
2025-10-17 18:06:44 +0200

    fix app.js : serve static files and add 404 page

--- app.js
+++ app.js
@@ -15,9 +15,14 @@ app.use(expressLayouts);
 app.set("layout", "layout");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 console.log("Static files served from:", path.join(__dirname, "public"));
 
 app.use("/", Router);
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Page Not Found" });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
